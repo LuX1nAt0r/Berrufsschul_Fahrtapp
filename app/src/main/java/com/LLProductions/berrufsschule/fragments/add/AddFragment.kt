71b1b8aaa3a374e0c4952a742d399353f30591ca
LuX1nAt0r <66088547+LuX1nAt0r@users.noreply.github.com>
2020-10-17 13:25:06 +0200

    added databinding

--- app/src/main/java/com/LLProductions/berrufsschule/fragments/add/AddFragment.kt
+++ app/src/main/java/com/LLProductions/berrufsschule/fragments/add/AddFragment.kt
@@ -5,17 +5,22 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
 import com.LLProductions.berrufsschule.R
+import com.LLProductions.berrufsschule.databinding.FragmentAddBinding
 
 
 class AddFragment : Fragment() {
+    lateinit var binding: FragmentAddBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_add, container, false)
+        //return inflater.inflate(R.layout.fragment_add, container, false)
+        binding = DataBindingUtil.inflate(inflater,R.layout.fragment_add, container, false)
+        return binding.root
     }
 
 }
--- app/src/main/java/com/LLProductions/berrufsschule/fragments/list/ListFragment.kt
+++ app/src/main/java/com/LLProductions/berrufsschule/fragments/list/ListFragment.kt
@@ -5,21 +5,24 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
 import com.LLProductions.berrufsschule.R
+import com.LLProductions.berrufsschule.databinding.FragmentListBinding
 
 
 class ListFragment : Fragment() {
+    lateinit var binding: FragmentListBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        val view = return inflater.inflate(R.layout.fragment_list, container, false)
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_list, container, false)
 
 
 
-        return view
+        return binding.root
     }
 
 
