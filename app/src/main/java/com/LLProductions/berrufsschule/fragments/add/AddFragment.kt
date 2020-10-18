abf7af6060aab25172d30c31d91179067340df19
LuX1nAt0r <66088547+LuX1nAt0r@users.noreply.github.com>
2020-10-18 14:01:03 +0200

    Insert Data to Database is working

--- app/src/main/java/com/LLProductions/berrufsschule/fragments/add/AddFragment.kt
+++ app/src/main/java/com/LLProductions/berrufsschule/fragments/add/AddFragment.kt
@@ -1,17 +1,28 @@
 package com.LLProductions.berrufsschule.fragments.add
 
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextUtils
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.databinding.DataBindingUtil
+import android.widget.Toast
+import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.fragment.findNavController
+
 import com.LLProductions.berrufsschule.R
-import com.LLProductions.berrufsschule.databinding.FragmentAddBinding
+import com.LLProductions.berrufsschule.data.User
+import com.LLProductions.berrufsschule.data.UserViewModel
+import kotlinx.android.synthetic.main.fragment_add.*
+import kotlinx.android.synthetic.main.fragment_add.view.*
+import kotlinx.android.synthetic.main.fragment_list.view.*
 
 
 class AddFragment : Fragment() {
-    lateinit var binding: FragmentAddBinding
+
+    private lateinit var mUserViewModel: UserViewModel
+
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -19,8 +30,39 @@ class AddFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         //return inflater.inflate(R.layout.fragment_add, container, false)
-        binding = DataBindingUtil.inflate(inflater,R.layout.fragment_add, container, false)
-        return binding.root
+    val view = inflater.inflate(R.layout.fragment_add, container, false)
+
+        mUserViewModel = ViewModelProvider(this).get(UserViewModel::class.java)
+
+
+        view.btnAdd.setOnClickListener {
+            insertDataToDatabse()
+        }
+
+        return view
     }
 
+    private fun insertDataToDatabse() {
+        val firstName = et_addFirtsName.text.toString()
+        val lastName = et_addLastName.text.toString()
+        val age = et_addAge.text
+
+        if(inputCheck(firstName, lastName, age)){
+            //Create User Object
+            val user = User(0,firstName, lastName, Integer.parseInt(age.toString()) )
+            //Add Data to Database
+            mUserViewModel.addUser(user)
+            Toast.makeText(requireContext(),"Successfully added!", Toast.LENGTH_LONG).show()
+            //Navigate Back to ListFragment
+            findNavController().navigate(R.id.action_addFragment_to_listFragment)
+
+        }else{
+            Toast.makeText(requireContext(),"Please fill out all fields", Toast.LENGTH_LONG).show()
+        }
+
+    }
+
+    private fun inputCheck(firstName: String, lastName: String, age: Editable): Boolean{
+        return !(TextUtils.isEmpty(firstName) && TextUtils.isEmpty(lastName) && age.isEmpty())
+    }
 }
--- app/src/main/java/com/LLProductions/berrufsschule/fragments/list/ListFragment.kt
+++ app/src/main/java/com/LLProductions/berrufsschule/fragments/list/ListFragment.kt
@@ -5,24 +5,27 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.databinding.DataBindingUtil
+
+import androidx.navigation.fragment.findNavController
 import com.LLProductions.berrufsschule.R
-import com.LLProductions.berrufsschule.databinding.FragmentListBinding
+import kotlinx.android.synthetic.main.fragment_list.view.*
 
 
 class ListFragment : Fragment() {
-    lateinit var binding: FragmentListBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_list, container, false)
+        val view = inflater.inflate(R.layout.fragment_list, container, false)
 
+        view.floatingActionButton.setOnClickListener {
+            findNavController().navigate(R.id.action_listFragment_to_addFragment)
+        }
 
+        return view
 
-        return binding.root
     }
 
 
